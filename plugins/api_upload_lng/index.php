a57eaf9a9c4c68a376e35f7cff59bda93abef4f3
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2018-03-29 10:09:28 +0200

    corrected path to config

--- plugins/api_upload_lng/index.php
+++ plugins/api_upload_lng/index.php
@@ -3,7 +3,7 @@
 require_once '../../Autoloader.php';
 
 // Path to RS config file
-$rsConfigPath = '/Users/ruud/Documents/MAMP/htdocs/resourcespace/include/config.php';
+$rsConfigPath = '/var/www/resourcespace/include/config.php';
 
 // Controllers MUST be initiased with path to RS config file!
 $upload = new RsApi\UploadController($rsConfigPath);
